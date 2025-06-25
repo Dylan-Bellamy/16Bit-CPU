99c17e8618896b3856aa9aec2788a467ffc969ca
Dylan-Bellamy <90278426+Dylan-Bellamy@users.noreply.github.com>
2025-06-25 16:48:35 -0400

    Commit 6: Debugging & Updating
    This commit has the finalized code and bdf files after debugging and testing the code.

--- mux.vhd
+++ mux.vhd
@@ -2,8 +2,8 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY mux16to4 IS
-	PORT (w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,
-		w12,w13,w14,w15 : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+	PORT (w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,wa,wb,
+		wc,wd,we,wf : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 			s		: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
 			f		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0));
 END mux16to4;
@@ -21,20 +21,20 @@ BEGIN
 			 w7 WHEN "0111",
 			 w8 WHEN "1000",
 			 w9 WHEN "1001",
-			 w10 WHEN "1010",
-			 w11 WHEN "1011",
-			 w12 WHEN "1100",
-			 w13 WHEN "1101",
-			 w14 WHEN "1110",
-			 w15 WHEN OTHERS;
+			 wa WHEN "1010",
+			 wb WHEN "1011",
+			 wc WHEN "1100",
+			 wd WHEN "1101",
+			 we WHEN "1110",
+			 wf WHEN "1111";
 END Behavoir;
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 PACKAGE mux16to4_package IS
 	COMPONENT mux16to4
-		PORT (w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,
-		w12,w13,w14,w15 : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		PORT (w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,wa,wb,
+		wc,wd,we,wf : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 				s		: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
 				f		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0));
 	END COMPONENT;
