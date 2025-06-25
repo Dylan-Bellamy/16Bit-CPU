49d1ef91dd625464875658e0b820f291c0cf1acd
Dylan-Bellamy <90278426+Dylan-Bellamy@users.noreply.github.com>
2025-06-25 16:49:48 -0400

    Commit 7: Added More Instructions
    Added more instructions in VHDL to the instruction memory component.

--- Instruction Memory.vhd
+++ Instruction Memory.vhd
@@ -18,7 +18,15 @@ architecture behavior of instruction is
 	"0100010001101010", -- NAND, r4 NAND r6 -> r10
 	"0101101000111111", -- NOR, r10 NOR r3 -> r15
 	"0110111111001000", -- AND, r15 AND r12 -> r8
-	"0111100010110000"); -- OR, r8 OR r13 -> r0
+	"0111100010110000", -- OR, r8 OR r13 -> r0
+	"1000000011101001", -- XOR, r0 XOR r14 -> r9
+	"1001100100000001", -- SLL, r9 << Imm -> r1
+	"1010101000001011", -- SRL, r12 >> Imm -> r11
+	"1011110100001101", -- INC, r13 + 1 -> r13
+	"1100111000001110", -- DEC, r14 - 1 -> r14
+	"1101101000111111", -- ASR, r14 >>> Imm -> r6
+	"1110011011000101", -- MUL, r6 * r12 -> r5
+	"1111110010110000"); -- CMP (Compare - set flags), r5 - r12 
 begin
 	instr_addr <= address(15 downto 0);
    opcode <= instr_data(to_integer(unsigned(instr_addr))) when address < x"0010" else x"0000";
